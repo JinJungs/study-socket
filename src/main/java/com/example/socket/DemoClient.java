91cbef91709d915807805d2e17454873bc66f4cb
jinjungs <nehciyz@gmail.com>
2021-08-27 17:06:55 +0900

    데이터 주고받기를 thread로 구현 & 웹서버 테스트 추가
    - thread는 Runnable을 구현한 익명클래스
    - 1. GET 으로 왔는지 형식 확인 -> 아니면 400 return
    - 2. 파일이 존재하는지 확인 -> 아니면 404 return
    - 3. 파일이 존재하면 내용을 읽고 return

--- src/main/java/com/example/socket/DemoClient.java
+++ src/main/java/com/example/socket/DemoClient.java
@@ -3,81 +3,86 @@ package com.example.socket;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
-import java.nio.charset.StandardCharsets;
 
 public class DemoClient {
 
     public static void main(String[] args) throws IOException {
-        DemoClient demoClient = new DemoClient();
-        demoClient.run();
-    }
 
-    void run() throws IOException {
-        System.out.println(":::::: Democlient Start ::::::");
+        // 1. 소켓 테스트
+        // String message = "apple";
+        // 2. 웹서버 테스트
+        String message = "GET 1.txt";
 
-        // 소켓 생성
-        Socket socket = new Socket();
 
-        // 주소 생성
-        SocketAddress address = new InetSocketAddress("127.0.0.1", 5050);
+        for (int i=0; i<10; i++){
 
-        // 주소에 해당하는 서버와 연결
-        socket.connect(address);
-        System.out.println(":::::: connected ::::::");
+            // 여러 클라이언트에서 들어온다는 가정하에 쓰레드로 처리
+            // 단순 반복문으로 처리하니 요청이 순차적으로 들어와서 쓰레드가 정상작동하는지 확인이 어려움
+            Thread thread = testThread(message);
+            thread.start();
 
-        try{
-            // 메세지 전송
-            send(socket);
-            System.out.println(":::::: send Message ::::::");
-
-            // 메세지 받기
-            receive(socket);
-            System.out.println(":::::: receive Message ::::::");
-
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void send(Socket socket) throws IOException {
-
-        // 전달할 String 생성
-        String a = "hello";
-
-        // byte로 변환
-        byte[] data = a.getBytes(StandardCharsets.UTF_8);
-
-        // 서버로 보낼 출력 스트림
-        OutputStream os = socket.getOutputStream();
-        
-        // 출력 스트림에 데이터를 쓰고
-        os.write(data);
-        
-        // 보냄
-        os.flush();
-
-    }
-
-    public static void receive(Socket socket) throws IOException {
-        // 수신 버퍼의 최대 사이즈
-        int maxBufferSize = 5;
-        // 버퍼 생성
-        byte[] recvBuffer = new byte[maxBufferSize];
-        // 서버로부터 받기 위한 입력 스트림
-        InputStream is = socket.getInputStream();
-        // 버퍼를 인자로 넣어 반환 값을 받음
-        int readSize = is.read(recvBuffer);
-
-        // 받아온 값이 0보다 클 때
-        if(readSize > 0){
-            // 받아온 byte를 String으로 변환
-            String a1 = new String(recvBuffer);
-            System.out.println(">>>>>> 응답받은 메세지 : " + a1 + " >>>>>>");
         }
 
     }
+
+    private static Thread testThread(String message) {
+        return new Thread( new Runnable() {
+
+            @Override
+            public void run() {
+                // 필요한 객체 생성
+                OutputStream os = null;
+                InputStream is = null;
+                Socket socket = null;
+
+                try{
+
+                    // 1. 소켓 생성
+                    socket = new Socket("127.0.0.1", 5050);
+                    System.out.println(":::::: connected ::::::");
+
+
+                    // 2. 데이터 전송
+                    byte[] data = message.getBytes(); // byte로 변환
+                    os = socket.getOutputStream();  // 서버로 보낼 출력 스트림
+                    os.write(data); // 출력 스트림에 데이터를 쓰고
+                    os.flush();   // 보냄
+                    System.out.println(":::::: send Message : " + message);
+
+
+                    // 3． 데이터 수신
+                    int maxBufferSize = 2000; // 수신 버퍼의 최대 사이즈
+                    byte[] recvBuffer = new byte[maxBufferSize]; // 버퍼 생성
+
+                    is = socket.getInputStream(); // 서버로부터 받기 위한 입력 스트림
+                    int readSize = is.read(recvBuffer); // 버퍼를 인자로 넣어 반환 값과 사이즈를 받음
+                    String recvMessage = new String(recvBuffer, 0, readSize); // 받아온 byte를 String으로 변환
+
+                    // 받아온 값이 0보다 클 때
+                    if(readSize > 0){
+                        System.out.println(":::::: receive Message : " + recvMessage);
+                    }
+
+                }catch (IOException e) {
+                    e.printStackTrace();
+
+                }finally {
+                    try {
+                        // 끝난 후에는 반드시 close
+                        os.close();
+                        is.close();
+                        socket.close();
+                        System.out.println(":::::::::::::::::: ended ::::::::::::::::::\n");
+
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
 
 }
--- src/main/java/com/example/socket/DemoServer.java
+++ src/main/java/com/example/socket/DemoServer.java
@@ -1,55 +1,194 @@
 package com.example.socket;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class DemoServer {
+
     public static void main(String[] args) throws IOException {
-        DemoServer demoServer = new DemoServer();
-        demoServer.run();
-    }
 
-    public void run() throws IOException {
+        // 초기화
+        ServerSocket ssk = null;
+        int seq = 0;
+        int port = 5050;
+
         try{
-            int port = 5050;
 
-            // 서버소켓을 생성
-            ServerSocket ssk = new ServerSocket(port);
+            // 서버소켓을 생성 - bind와 listen을 포함
+            ssk = new ServerSocket(port);
 
+            // 요청이 들어올 때마다 반복 수행
             while (true) {
+
                 // 1. 연결
                 System.out.println("****** 접속대기중 ******");
                 Socket socket = ssk.accept();
-                System.out.println(ssk.getInetAddress() +" 로부터 연결 요청이 들어옴");
+                System.out.println(socket.getInetAddress() +" 로부터 연결 요청이 들어옴");
 
-                // 2. 데이터 받기
-                InputStream is = socket.getInputStream();
-                byte[] bytes = new byte[2000];
+                // 데이터 받기 및 전송은 쓰레드에서 처리
+                // 1. 소켓 테스트
+                //Thread thread = testSocket(seq, socket);
+                // 2. 웹서버 테스트
+                Thread thread = testWebServer(seq, socket);
 
-                int readByteCount = is.read(bytes);
+                thread.start();
 
-                if (readByteCount > 0) {
-                    System.out.println("클라이언트로부터 데이터 수신");
-                    sendData(bytes, socket);
-                }
-                System.out.println("****** 재전송완료 ******");
+                seq++;
+
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        finally {
+            try {
+                ssk.close();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+
+    }
+    
+    // 1. 소켓 테스트
+    private static Thread testSocket(int seq, Socket socket) {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(seq + " thread start");
+                try {
+
+                    Thread.sleep(1000);
+
+                    // 2. 데이터 받기
+                    String recvMessage = readData(socket);
+
+                    // 3. 데이터 재전송
+                    sendData(socket, recvMessage);
+
+                }catch(Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println(seq+" thread end.");
+            }
+        });
     }
 
-    public void sendData(byte[] bytes, Socket socket) throws IOException {
-        try {
-            OutputStream os = socket.getOutputStream();
-            os.write(bytes);
-            os.flush();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+
+    // 2. 웹서버 테스트
+    private static Thread testWebServer(int seq, Socket socket) {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(seq + " thread start");
+                try {
+
+                    Thread.sleep(1000);
+
+                    /*
+                     * 1. GET 으로 왔는지 형식 확인 -> 아니면 400 return
+                     * 2. 파일이 존재하는지 확인 -> 아니면 404 return
+                     * 3. 파일이 존재하면 내용을 읽고 return
+                     * */
+
+                    // 1. 데이터 받기
+                    String recvMessage = readData(socket);
+
+                    // message null 체크
+                    if (recvMessage == null) {
+                        sendData(socket, "400");
+                        return;
+                    }
+
+                    // 받은 메세지를 GET 과 받은 파일이름으로 구분
+                    String[] headerArr = recvMessage.split(" ");
+                    String httpMethod = headerArr[0];
+                    String recvFileName = headerArr[1];
+
+                    // (1) GET 으로 왔는지 형식 확인
+                    if (!"GET".equals(httpMethod)) {
+                        sendData(socket, "400");
+                        return;
+                    }
+
+                    // (2) 파일 이름 null 체크
+                    if (recvFileName == null | "".equals(recvFileName)) {
+                        sendData(socket, "404");
+                        return;
+                    }
+
+                    // (3) 파일이 존재하는지 확인
+                    String dirPath = "C:\\source\\ToyProjectWorkspace\\toy_socket\\src\\main\\resources";
+                    File dir = new File(dirPath);
+                    File[] files = dir.listFiles();
+                    boolean isFileExist = false;
+                    for (File file : files) {
+                        if (recvFileName.equals(file.getName())) {
+                            isFileExist = true;
+                            break;
+                        }
+                    }
+
+                    // (4) 파일 내용 읽기
+                    if (isFileExist) {
+                        byte[] fileBytes = Files.readAllBytes(Paths.get(dirPath + "\\" + recvFileName));
+                        sendData(socket, new String(fileBytes));
+                        return;
+                    }
+
+                    // (5) 파일이 없다면 404 전송
+                    sendData(socket, "404");
+                    return;
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(seq + " thread end.");
+            }
+
+        });
     }
+
+
+    ///////////////////////////////////////////////// 내부 메서드 ////////////////////////////////////////////////////////
+
+    // 데이터 수신
+    private static String readData(Socket socket) throws IOException {
+        InputStream is = socket.getInputStream();
+
+        byte[] bytes = new byte[2000];
+
+        // 읽은 byte의 크기 반환
+        int readByteCount = is.read(bytes);
+
+        // 수신한 데이터 - byte 크기만큼 자른다.
+        String recvMessage = new String(bytes, 0, readByteCount);
+
+        System.out.println("클라이언트로부터 데이터 수신 :\n" + recvMessage);
+
+        return recvMessage;
+    }
+
+    // 테이터 전송
+    private static void sendData(Socket socket, String sendMessage) throws IOException {
+        OutputStream os = socket.getOutputStream();
+        os.write(sendMessage.getBytes());
+        os.flush();
+
+        System.out.println("클라이언트로 보낸 메세지 : " + sendMessage);
+        System.out.println("****************** 전송완료 ******************\n");
+
+    }
+    
+
+
 }
